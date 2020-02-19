f134572152b5fa4ab491dbb7d4b5b904ca4ca2db
Henry Brown <h@0ti.me>
2020-02-19 16:36:16 -0300

    Add support for location

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,3 +1,9 @@
 module.exports = {
+  CLASSES: {
+    HEADER_LOCATION: 'header-location',
+  },
   DEFAULT_SKILLS_PER_LINE: 4,
+  SELECTORS: {
+    HEADER_LOCATION: 'about.location',
+  },
 };
--- src/lib/html/resume/header.js
+++ src/lib/html/resume/header.js
@@ -1,16 +1,31 @@
 const a = require('../a');
 const div = require('../div');
+const {get, has} = require('lodash');
 const tel = require('../tel');
 const mailto = require('../mailto');
+
+const {CLASSES, SELECTORS} = require('../../constants');
+
+const getIdentity = resume => {
+  const fields = [];
+
+  fields.push(div('header-name')(resume.about.name));
+  fields.push(div('header-subtitle')(resume.about.subtitle));
+
+  if (has(resume, SELECTORS.HEADER_LOCATION)) {
+    fields.push(
+      div(CLASSES.HEADER_LOCATION)(get(resume, SELECTORS.HEADER_LOCATION)),
+    );
+  }
+
+  return div('col header-identity')(fields);
+};
 
 const getHeader = resume =>
   div('block')(
     div('contents')(
       div('row header-row')([
-        div('col header-identity')([
-          div('header-name')(resume.about.name),
-          div('header-subtitle')(resume.about.subtitle),
-        ]),
+        getIdentity(resume),
         div('col header-contact')([
           div('header-phone')(tel(resume.about.phone)),
           div('header-email')(mailto(resume.about.email)),
